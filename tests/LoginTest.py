2c6090e7b9d78562236100b9c17be605abef9a86
Tatevik Navasardyan <tatev.navasardyan.k@gmail.com>
2020-05-25 22:04:35 +0400

    chromedriver

--- tests/LoginTest.py
+++ tests/LoginTest.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome() #executable_path='C:/webdrivers/chromedriver'
+driver = webdriver.Chrome()
 
 driver.get('http://the-internet.herokuapp.com/login')
 
